0dd53281c49828480dce05a4170126edf84366b3
stte61420 <stte61420@users.noreply.github.com>
2026-09-22 11:51:44 +0000

    dash: guard dash-brightness behind DASH_ENABLE_BRIGHTNESS_HOOKS for AOSP 16

--- display/brightness/brightness.mk
+++ display/brightness/brightness.mk
@@ -1,7 +1,10 @@
 # Copyright (C) 2026 GitHub @YorokobiMaster
 # SPDX-License-Identifier: Apache-2.0
+
+ifeq ($(DASH_ENABLE_BRIGHTNESS_HOOKS),true)
 PRODUCT_PACKAGES += dash-brightness
 PRODUCT_SYSTEM_SERVER_JARS_EXTRA += system_ext:dash-brightness
+endif
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/display/brightness/config/display_id_4627039422300187648.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/displayconfig/display_id_4627039422300187648.xml \
